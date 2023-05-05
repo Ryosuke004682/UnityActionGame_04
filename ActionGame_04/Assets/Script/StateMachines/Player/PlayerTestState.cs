7f622f67eee6ec6c851c6795b459cf3b6fa3dfd5
ryousuke <83821881+Ryosuke004682@users.noreply.github.com>
2023-05-06 02:52:15 +0900

    モーションの追加、カメラメインの移動に変更

--- ActionGame_04/Assets/Script/StateMachines/Player/PlayerTestState.cs
+++ ActionGame_04/Assets/Script/StateMachines/Player/PlayerTestState.cs
@@ -20,18 +20,19 @@ public class PlayerTestState : PlayerBaseState
 
     public override void Tick(float deltaTime)
     {
-        Vector3 movement = new Vector3();
-
-        //プレイヤーの移動処理
-        movement.x = stateMachine.InputRender.v2_MovementValue.x;
-        movement.y = 0;
-        movement.z = stateMachine.InputRender.v2_MovementValue.y;
+        Vector3 movement = CalculateMovement();
 
         stateMachine.Controller.Move(movement * stateMachine.MovementSpeed * deltaTime);
 
-        if (stateMachine.InputRender.v2_MovementValue == Vector2.zero) { return; }
+        if (stateMachine.InputRender.v2_MovementValue == Vector2.zero)
+        {
+            stateMachine.Animator.SetFloat("FreeLookSpeed", 0.0f , 0.1f , deltaTime);
+            return;
+        }
 
-        stateMachine.transform.rotation = Quaternion.LookRotation(new Vector3(-movement.z , movement.y , movement.x));
+        stateMachine.Animator.SetFloat("FreeLookSpeed", 2.0f , 0.1f , deltaTime);
+
+        stateMachine.transform.rotation = Quaternion.LookRotation(movement);
     }
 
     public override void Exit()
@@ -39,6 +40,22 @@ public class PlayerTestState : PlayerBaseState
         
     }
 
+    //プレイヤーの移動処理
+    //カメラの相対移動
+    private Vector3 CalculateMovement()
+    {
+        var forward = stateMachine.MainCameraTransform.forward;
+        var right   = stateMachine.MainCameraTransform.right;
+
+        forward.y = 0;
+        right  .y = 0;
+
+        forward.Normalize();
+        right  .Normalize();
+
+        return forward * stateMachine.InputRender.v2_MovementValue.y +
+               right   * stateMachine.InputRender.v2_MovementValue.x  ;
+    }
 
     /*
     命名規則
@@ -57,6 +74,8 @@ public class PlayerTestState : PlayerBaseState
     float・・・・・・f_～～～
     bool ・・・・・・b_～～～
     const・・・・・・全て大文字(単語間はアンダースコアで繋ぐ)
+
+    ローカルは特に規定は無し。
 
 
     イベント関数 ・・on～～～()
